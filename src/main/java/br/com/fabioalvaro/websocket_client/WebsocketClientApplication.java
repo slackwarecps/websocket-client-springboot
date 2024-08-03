cf9bb51f461d9dc38cefb63da933e7870a4a49f5
fabiopereiraz <fabio.pereira@icarros.com.br>
2024-08-03 12:33:04 -0300

    upload inicial

---
+++ src/main/java/br/com/fabioalvaro/websocket_client/WebsocketClientApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabioalvaro.websocket_client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsocketClientApplication.class, args);
+	}
+
+}
